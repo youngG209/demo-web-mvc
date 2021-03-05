aa0434ef514781439ca9bbb443c188d71d84af73
youngG209 <young209@mcocoa.com>
2021-03-05 15:46:57 +0900

    @ModelAttribute

--- src/main/java/com/demowebmvc/EventsController.java
+++ src/main/java/com/demowebmvc/EventsController.java
@@ -14,6 +14,16 @@ import java.util.Optional;
 @Controller
 public class EventsController {
 
+//    @ModelAttribute의 다른 용법
+//        ● @RequestMapping을 사용한 핸들러 메소드의 아규먼트에 사용하기 (이미 살펴봤습니다.)
+//        ● @Controller 또는 @ControllerAdvice (이 애노테이션은 뒤에서 다룹니다.)를 사용한 클래스에서 모델 정보를 초기화 할 때 사용한다.
+//        ● @RequestMapping과 같이 사용하면 해당 메소드에서 리턴하는 객체를 모델에 넣어 준다.
+//            ○ RequestToViewNameTranslator
+    @ModelAttribute
+    public void categories(Model model) {
+        model.addAttribute("categories", List.of("study","study","study","study"));
+    }
+
     @GetMapping("/event/form")
     public String eventForm(Model model) {
         Event newEvent = new Event();
